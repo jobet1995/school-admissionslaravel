fa6e721e4b4f62e722b2fcb2ad9a57304a2396d1
Jobet Casquejo <87087925+jobet1995@users.noreply.github.com>
2024-04-21 08:17:57 +0000

    added

--- config/database.php
+++ config/database.php
@@ -22,6 +22,7 @@ return [
         ],
         'pgsql' => [
             'driver' => 'pgsql',
+            'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'forge'),
@@ -29,6 +30,7 @@ return [
             'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'prefix' => '',
+            'prefix_indexes' => true,
             'schema' => 'public',
             'sslmode' => 'prefer'
         ],
--- routes/web.php
+++ routes/web.php
@@ -1,66 +1,31 @@
 <?php
 
-use App\Http\Controllers\Auth\LoginController;
-use App\Http\Controllers\SystemController;
-use App\Http\Controllers\UserAccountController;
-use App\Http\Controllers\UserRoleController;
-use App\Http\Controllers\AdmissionDecisionController;
-use App\Http\Controllers\ApplicationsController;
-use App\Http\Controllers\ClassController;
-use App\Http\Controllers\CourseController;
-use App\Http\Controllers\EnrollmentController;
-use App\Http\Controllers\FacultyController;
-use App\Http\Controllers\ProgramController;
-use App\Http\Controllers\StudentController;
-use App\Http\Controllers\RoleController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
 
-Route::get('/', [ SystemController::class, 'default' ]);
-Route::group(['prefix' => 'admin'], function() {
-Auth::routes();
-Route::group(['middleware' => 'auth'], function() {
-    Route::get('/', function () { return redirect('admin/home'); });
-    Route::get('/home', [ SystemController::class, 'home' ]);
-    Route::get('/profile', [ SystemController::class, 'profile' ]);
-    Route::post('/updateProfile', [ SystemController::class, 'updateProfile' ]);
-    Route::middleware('role:ADMIN')->resource('/userAccounts', UserAccountController::class);
-    Route::middleware('role:ADMIN,USER')->resource('/userRoles', UserRoleController::class);
-    Route::middleware('role:ADMIN,USER')->resource('/admissionDecisions', AdmissionDecisionController::class);
-    Route::middleware('role:ADMIN,USER')->resource('/applicationses', ApplicationsController::class);
-    Route::middleware('role:ADMIN,USER')->resource('/classes', ClassController::class);
-    Route::middleware('role:ADMIN,USER')->resource('/courses', CourseController::class);
-    Route::middleware('role:ADMIN,USER')->resource('/enrollments', EnrollmentController::class);
-    Route::middleware('role:ADMIN,USER')->resource('/faculties', FacultyController::class);
-    Route::middleware('role:ADMIN,USER')->resource('/programs', ProgramController::class);
-    Route::middleware('role:ADMIN,USER')->resource('/students', StudentController::class);
-    Route::middleware('role:ADMIN,USER')->resource('/roles', RoleController::class);
-    Route::middleware('role:ADMIN,USER')->get('/userRoles/{user_id}/{role_id}', [ UserRoleController::class, 'show' ]);
-    Route::middleware('role:ADMIN,USER')->get('/userRoles/{user_id}/{role_id}/edit', [ UserRoleController::class, 'edit' ]);
-    Route::middleware('role:ADMIN,USER')->patch('/userRoles/{user_id}/{role_id}', [ UserRoleController::class, 'update' ]);
-    Route::middleware('role:ADMIN,USER')->delete('/userRoles/{user_id}/{role_id}', [ UserRoleController::class, 'destroy' ]);
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
 });
-Route::get('/logout', [ LoginController::class, 'logout' ]);
-Route::get('/resetPassword', [ LoginController::class, 'resetPassword' ]);
-Route::post('/resetPassword', [ LoginController::class, 'resetPasswordPost' ]);
-Route::get('/changePassword/{token}', [ LoginController::class, 'changePassword' ]);
-Route::post('/changePassword/{token}', [ LoginController::class, 'changePasswordPost' ]);
-Route::get('/stack', [ SystemController::class, 'stack' ]);
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+require __DIR__.'/auth.php';
